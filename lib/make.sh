0d88b7856f603300531f1b2a92be3e209bf3b447
Jasonpang0099 <38741043+Jasonpang0099@users.noreply.github.com>
2018-09-02 22:13:36 +1200

    Update make.sh

--- lib/make.sh
+++ lib/make.sh
@@ -1,10 +1,11 @@
 TF_INC=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
+TF_LIB=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
 
 CUDA_PATH=/usr/local/cuda/
 CXXFLAGS=''
 
 if [[ "$OSTYPE" =~ ^darwin ]]; then
-	CXXFLAGS+='-undefined dynamic_lookup'
+	CXXFLAGS+='-D_MWAITXINTRIN_H_INCLUDED'
 fi
 
 cd roi_pooling_layer
@@ -14,9 +15,9 @@ if [ -d "$CUDA_PATH" ]; then
 		-I $TF_INC -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC $CXXFLAGS \
 		-arch=sm_37
 
-	g++ -std=c++11 -shared -o roi_pooling.so roi_pooling_op.cc \
-		roi_pooling_op.cu.o -I $TF_INC  -D GOOGLE_CUDA=1 -fPIC $CXXFLAGS \
-		-lcudart -L $CUDA_PATH/lib64 -02 -D_GLIBCXX_USE_CXX11_ABI=0 -L$TF_INC -ltensorflow_framework
+	g++ -std=c++11 -shared -o roi_pooling.so roi_pooling_op.cc -D_GLIBCXX_USE_CXX11_ABI=0 \
+		roi_pooling_op.cu.o -I $TF_INC -L$TF_LIB -ltensorflow_framework -D GOOGLE_CUDA=1 -fPIC $CXXFLAGS \
+		-lcudart -L $CUDA_PATH/lib64   
 else
 	g++ -std=c++11 -shared -o roi_pooling.so roi_pooling_op.cc \
 		-I $TF_INC -fPIC $CXXFLAGS
