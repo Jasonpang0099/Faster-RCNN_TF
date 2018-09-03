194b25a0a3e61141cb02458830a825fd08afa07b
Jasonpang0099 <38741043+Jasonpang0099@users.noreply.github.com>
2018-09-03 12:41:29 +1200

    Update make.sh

--- lib/make.sh
+++ lib/make.sh
@@ -1,5 +1,7 @@
 TF_INC=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
-#TF_LIB=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
+TF_LIB=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
+TF_CFLAGS=( $(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_compile_flags()))') )
+TF_LFLAGS=( $(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_link_flags()))') )
 
 CUDA_PATH=/usr/local/cuda/
 CXXFLAGS=''
@@ -13,10 +15,12 @@ cd roi_pooling_layer
 if [ -d "$CUDA_PATH" ]; then
 	nvcc -std=c++11 -c -o roi_pooling_op.cu.o roi_pooling_op_gpu.cu.cc \
 		-I $TF_INC -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC $CXXFLAGS \
-		-arch=sm_50
+		-arch=sm_37 -L$TF_LIB -ltensorflow_framework\
+　　　　　　　　　　　　　　　　              -D_GLIBCXX_USE_CXX11ABI=0
 
 	g++ -std=c++11 -shared -o roi_pooling.so roi_pooling_op.cc -D_GLIBCXX_USE_CXX11_ABI=0 \
 		roi_pooling_op.cu.o -I $TF_INC   -D GOOGLE_CUDA=1 -fPIC $CXXFLAGS \
+		${TF_CFLAGS[@]} ${TF_LFLAGS[@]} -O2 \
 		-lcudart -L $CUDA_PATH/lib64   
 else
 	g++ -std=c++11 -shared -o roi_pooling.so roi_pooling_op.cc \
